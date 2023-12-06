a8c416b30b2687f118bf737345d93e9fd6864890
Julia Jomon <juliajomon12@gmail.com>
2023-12-06 21:16:48 +0530

    Update postfix.c

--- postfix.c
+++ postfix.c
@@ -1,107 +1,99 @@
 /* JULIA JOMON
     CU-30   */
-
+    
 #include <stdio.h>
-#include <stdlib.h>
-#define MAX_SIZE 20
-int  top = -1 ;
-int Stack[MAX_SIZE] ;
-int item;
+#define MAX_SIZE 10
+int Queue[MAX_SIZE];
+int front = -1;
+int rear = -1;
 
-void push(int item)
+void enqueue(int item)
 {
-    if(top == MAX_SIZE -1)
-        printf("full");
-    else
-        top=top+1;
-        Stack[top]=item;
-}
-int pop()
-{
-    
-    if(top == -1){
-		printf("Empty\n");
-		return 0 ;
-	}
-        
-    else
-        item = Stack[top];
-        top=top-1;
-        return item;
-}
-
-int peek()
-{
-    if(top == -1){
-		printf("empty\n");
-        exit(1);
-		
-	}
-        
-    else
-        return Stack[top];
-}
-
-void postfix(char pos)
-{   
-    int op,x,y;
-        if(pos=='+'||pos=='-' || pos=='/' || pos=='*'  || pos == '$'){
-            float cal;
-            switch (pos)
-            {
-            case '+':
-                y = pop();
-                x = pop();
-                cal =(x+y);
-                push(cal);
-                break;
-
-            case '-' :
-                y = pop();
-                x = pop();
-                cal = x-y;
-                push(cal);
-                break;
-
-            case '*' :
-                y = pop();
-                x = pop();
-                cal =x*y;
-                push(cal);
-                break;
-
-            case '/' :
-                y = pop();
-                x = pop();
-                cal = x/y;
-                push(cal);
-                break;
-
-            case '$':
-            printf("\nEvaluation result = %d", peek());
-                break;
-            
-            default:
-                break;
-            }
-        }
-        else if (pos >= '0' && pos <= '9')
-             push(pos- '0' );
-    
-        
-
-}
-
-int main()
-{  
-   char post[MAX_SIZE];
-   int i =0;
-   printf("enter postfix expression and end with $: ");
-   do{
-       scanf(" %c",&post[i]);
-       printf("%c",post[i]);
-       postfix(post[i]);
-       i++;
-   }while(post[i-1]!='$');
+   if((rear+1)%MAX_SIZE== front){
+       printf("Queue is full\n");
+       return ;
+   }
+   else if (front==-1 && rear == -1)
+   {
+    front= rear=0;
+    Queue[rear]=item;
+   }
    
+   else{
+       rear = (rear + 1)%MAX_SIZE;
+       Queue[rear] = item;
+       printf("%d",item);
+   }
+}
+
+
+void dequeue()
+{
+    int item;
+    if(front == -1 && rear == -1){
+        printf("Queue is empty\n");
+        return;
+    }
+    else{
+        item = Queue[front];
+        if(front == rear){
+            front = -1;
+            rear = -1;
+        } 
+        else {
+            front = (front + 1) %MAX_SIZE;
+        }
+        printf("Deleted : %d\n",item);
+    }
+
+}
+
+void display()
+{   
+    int i = front;
+    if(front == -1 && rear == -1 ){
+        printf("Queue is empty\n");
+        return;
+    }
+    else
+        while(i <=rear){
+            printf("%d ",Queue[i]);
+            i++;
+        }
+}
+int main()
+{
+    int opp,item;
+    char ch = 'y';
+    do
+    {   
+        printf("1.Enqueue\n");
+        printf("2.Dequeue\n");
+        printf("3.Display\n");
+        scanf("%d",&opp);
+
+        switch (opp)
+        {
+        case 1 :
+            printf("Enter value to push: ");
+            scanf("%d",&item);
+            enqueue(item);
+            break;
+
+        case 2 :
+            dequeue();
+            break;
+
+        case 3 :
+            display();
+            break;
+
+        default:
+            break;
+        }
+        printf("Do you want to continue(y/n) ");
+        scanf("%s",&ch);
+    }while (ch == 'y');
+    return 0;   
+
 }
